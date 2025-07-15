70119fb0510013a62a4eb9065d7f63ddd268946a
mohamedkulane <mohankulane@gmail.com>
2025-07-15 09:33:49 +0300

    waxabaan kabadaly

--- main.js
+++ main.js
@@ -2,17 +2,38 @@ const weightInput = document.querySelector("#weight");
 const heightInput = document.querySelector("#height");
 const calcBtn = document.querySelector("#calculate");
 const resultBox = document.querySelector("#result");
+const btnreset=document.querySelector("#Reset")
+const icon=document.querySelector("#icon")
 
 calcBtn.addEventListener("click",()=>{
     const weight = parseFloat(weightInput.value);
     const height = parseFloat(heightInput.value);
     const bmi = weight / (height * height);
+          if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+          resultBox.textContent = "Fadlan geli xog sax ah.";
+          resultBox.style.color="red"
+          return;
+        }
      if (bmi < 16) {
-          resultBox.textContent = ("BMI waa:"+ " " +bmi.toFixed(1) +" Under weight");
+          resultBox.textContent = "Under weigth";
     } else if (bmi < 28) {
-      resultBox.textContent = ("BMI waa:"+ " " +bmi.toFixed(1) +" Normal");
+      resultBox.textContent = "Normal";
     } else {
-          resultBox.textContent = ("BMI waa:"+ " " +bmi.toFixed(1) +" Over weight");
+          resultBox.textContent = "Over weigth";
         }
     resultBox.classList.add("text-green-600");
+    if(bmi<16){
+      icon.innerHTML="💪"
+    }else if(bmi<28){
+      icon.innerHTML="👍"
+    }
+    else{
+      icon.innerHTML="😱"
+    }
+})
+btnreset.addEventListener("click", ()=>{
+    resultBox.textContent=""
+    weight.textContent=""
+    height.innerHTML="none"
+    icon.textContent=""
 })
